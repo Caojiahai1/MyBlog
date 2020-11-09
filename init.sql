a2a456dc0fa29607205c7001dcdd1f1950848729
Yanliang <18652550182@163.com>
2020-11-10 00:14:27 +0800

    初始化脚本

---
+++ init.sql
@@ -0,0 +1,223 @@
+/*
+SQLyog Ultimate v12.3.1 (64 bit)
+MySQL - 5.5.34 : Database - my_blog_db
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`my_blog_db` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `my_blog_db`;
+
+/*Table structure for table `generator_test` */
+
+DROP TABLE IF EXISTS `generator_test`;
+
+CREATE TABLE `generator_test` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `test` varchar(100) NOT NULL COMMENT '测试字段',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `generator_test` */
+
+/*Table structure for table `jdbc_test` */
+
+DROP TABLE IF EXISTS `jdbc_test`;
+
+CREATE TABLE `jdbc_test` (
+  `type` varchar(100) DEFAULT NULL COMMENT '类型',
+  `name` varchar(100) DEFAULT NULL COMMENT '名称'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `jdbc_test` */
+
+insert  into `jdbc_test`(`type`,`name`) values 
+('com.zaxxer.hikari.HikariDataSource','hikari数据源'),
+('org.apache.commons.dbcp2.BasicDataSource','dbcp2数据源'),
+('test','测试类'),
+('类别2','测试类2');
+
+/*Table structure for table `tb_admin_user` */
+
+DROP TABLE IF EXISTS `tb_admin_user`;
+
+CREATE TABLE `tb_admin_user` (
+  `admin_user_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '管理员id',
+  `login_user_name` varchar(50) NOT NULL COMMENT '管理员登陆名称',
+  `login_password` varchar(50) NOT NULL COMMENT '管理员登陆密码',
+  `nick_name` varchar(50) NOT NULL COMMENT '管理员显示昵称',
+  `locked` tinyint(4) DEFAULT '0' COMMENT '是否锁定 0未锁定 1已锁定无法登陆',
+  PRIMARY KEY (`admin_user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+/*Data for the table `tb_admin_user` */
+
+insert  into `tb_admin_user`(`admin_user_id`,`login_user_name`,`login_password`,`nick_name`,`locked`) values 
+(1,'admin','e10adc3949ba59abbe56e057f20f883e','颜良',0);
+
+/*Table structure for table `tb_blog` */
+
+DROP TABLE IF EXISTS `tb_blog`;
+
+CREATE TABLE `tb_blog` (
+  `blog_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '博客表主键id',
+  `blog_title` varchar(200) NOT NULL COMMENT '博客标题',
+  `blog_sub_url` varchar(200) NOT NULL COMMENT '博客自定义路径url',
+  `blog_cover_image` varchar(200) NOT NULL COMMENT '博客封面图',
+  `blog_content` mediumtext NOT NULL COMMENT '博客内容',
+  `blog_category_id` int(11) NOT NULL COMMENT '博客分类id',
+  `blog_category_name` varchar(50) NOT NULL COMMENT '博客分类(冗余字段)',
+  `blog_tags` varchar(200) NOT NULL COMMENT '博客标签',
+  `blog_status` tinyint(4) NOT NULL DEFAULT '0' COMMENT '0-草稿 1-发布',
+  `blog_views` bigint(20) NOT NULL DEFAULT '0' COMMENT '阅读量',
+  `enable_comment` tinyint(4) NOT NULL DEFAULT '0' COMMENT '0-允许评论 1-不允许评论',
+  `is_deleted` tinyint(4) NOT NULL DEFAULT '0' COMMENT '是否删除 0=否 1=是',
+  `create_time` datetime NOT NULL DEFAULT '1949-10-01 00:00:00' COMMENT '添加时间',
+  `update_time` datetime DEFAULT '1949-10-01 00:00:00' COMMENT '修改时间',
+  PRIMARY KEY (`blog_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+
+/*Data for the table `tb_blog` */
+
+/*Table structure for table `tb_blog_category` */
+
+DROP TABLE IF EXISTS `tb_blog_category`;
+
+CREATE TABLE `tb_blog_category` (
+  `category_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '分类表主键',
+  `category_name` varchar(50) NOT NULL COMMENT '分类的名称',
+  `category_icon` varchar(50) NOT NULL COMMENT '分类的图标',
+  `category_rank` int(11) NOT NULL DEFAULT '1' COMMENT '分类的排序值 被使用的越多数值越大',
+  `is_deleted` tinyint(4) NOT NULL DEFAULT '0' COMMENT '是否删除 0=否 1=是',
+  `create_time` datetime NOT NULL DEFAULT '1949-10-01 00:00:00' COMMENT '创建时间',
+  PRIMARY KEY (`category_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8;
+
+/*Data for the table `tb_blog_category` */
+
+/*Table structure for table `tb_blog_comment` */
+
+DROP TABLE IF EXISTS `tb_blog_comment`;
+
+CREATE TABLE `tb_blog_comment` (
+  `comment_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键id',
+  `blog_id` bigint(20) NOT NULL DEFAULT '0' COMMENT '关联的blog主键',
+  `commentator` varchar(50) NOT NULL DEFAULT '' COMMENT '评论者名称',
+  `email` varchar(100) NOT NULL DEFAULT '' COMMENT '评论人的邮箱',
+  `website_url` varchar(50) NOT NULL DEFAULT '' COMMENT '网址',
+  `comment_body` varchar(200) NOT NULL DEFAULT '' COMMENT '评论内容',
+  `comment_create_time` datetime NOT NULL COMMENT '评论提交时间',
+  `commentator_ip` varchar(20) NOT NULL DEFAULT '' COMMENT '评论时的ip地址',
+  `reply_body` varchar(200) NOT NULL DEFAULT '' COMMENT '回复内容',
+  `reply_create_time` datetime NOT NULL COMMENT '回复时间',
+  `comment_status` tinyint(4) NOT NULL DEFAULT '0' COMMENT '是否审核通过 0-未审核 1-审核通过',
+  `is_deleted` tinyint(4) DEFAULT '0' COMMENT '是否删除 0-未删除 1-已删除',
+  PRIMARY KEY (`comment_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8;
+
+/*Data for the table `tb_blog_comment` */
+
+/*Table structure for table `tb_blog_tag` */
+
+DROP TABLE IF EXISTS `tb_blog_tag`;
+
+CREATE TABLE `tb_blog_tag` (
+  `tag_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '标签表主键id',
+  `tag_name` varchar(100) NOT NULL COMMENT '标签名称',
+  `is_deleted` tinyint(4) NOT NULL DEFAULT '0' COMMENT '是否删除 0=否 1=是',
+  `create_time` datetime NOT NULL DEFAULT '1949-10-01 00:00:00' COMMENT '创建时间',
+  PRIMARY KEY (`tag_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=141 DEFAULT CHARSET=utf8;
+
+/*Data for the table `tb_blog_tag` */
+
+/*Table structure for table `tb_blog_tag_relation` */
+
+DROP TABLE IF EXISTS `tb_blog_tag_relation`;
+
+CREATE TABLE `tb_blog_tag_relation` (
+  `relation_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '关系表id',
+  `blog_id` bigint(20) NOT NULL COMMENT '博客id',
+  `tag_id` int(11) NOT NULL COMMENT '标签id',
+  `create_time` datetime DEFAULT NULL COMMENT '添加时间',
+  PRIMARY KEY (`relation_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=313 DEFAULT CHARSET=utf8;
+
+/*Data for the table `tb_blog_tag_relation` */
+
+/*Table structure for table `tb_config` */
+
+DROP TABLE IF EXISTS `tb_config`;
+
+CREATE TABLE `tb_config` (
+  `config_name` varchar(100) NOT NULL DEFAULT '' COMMENT '配置项的名称',
+  `config_value` varchar(200) NOT NULL DEFAULT '' COMMENT '配置项的值',
+  `create_time` datetime NOT NULL COMMENT '创建时间',
+  `update_time` datetime NOT NULL COMMENT '修改时间',
+  PRIMARY KEY (`config_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `tb_config` */
+
+insert  into `tb_config`(`config_name`,`config_value`,`create_time`,`update_time`) values 
+('footerAbout','your personal blog. have fun.','2018-11-11 20:33:23','2019-10-30 03:08:57'),
+('footerCopyRight','苏公网安备 32010202010680号','2018-11-11 20:33:31','2019-10-30 03:08:57'),
+('footerICP','苏ICP备19056169号','2018-11-11 20:33:27','2019-10-30 03:08:57'),
+('footerPoweredBy','2019 颜良','2018-11-11 20:33:36','2019-10-30 03:08:57'),
+('footerPoweredByURL','https://github.com/Caojiahai1','2018-11-11 20:33:39','2019-10-30 03:08:57'),
+('websiteDescription','人生之旅会有黄道吉日，也会有平淡时辰。<br>There are big days and there are small days.','2018-11-11 20:33:04','2019-10-30 03:08:52'),
+('websiteIcon','/admin/dist/img/ylLogo.png','2018-11-11 20:33:11','2019-10-30 03:08:52'),
+('websiteLogo','/admin/dist/img/ylLogo.png','2018-11-11 20:33:08','2019-10-30 03:08:52'),
+('websiteName','YanLiang','2018-11-11 20:33:01','2019-10-30 03:08:52'),
+('yourAvatar','https://avatars2.githubusercontent.com/u/29559587?s=460&v=4','2018-11-11 20:33:14','2019-10-30 03:08:54'),
+('yourEmail','18652550182@163.com','2018-11-11 20:33:17','2019-10-30 03:08:54'),
+('yourName','颜良','2018-11-11 20:33:20','2019-10-30 03:08:54');
+
+/*Table structure for table `tb_link` */
+
+DROP TABLE IF EXISTS `tb_link`;
+
+CREATE TABLE `tb_link` (
+  `link_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '友链表主键id',
+  `link_type` tinyint(4) NOT NULL DEFAULT '0' COMMENT '友链类别 0-友链 1-推荐 2-个人网站',
+  `link_name` varchar(50) NOT NULL COMMENT '网站名称',
+  `link_url` varchar(100) NOT NULL COMMENT '网站链接',
+  `link_description` varchar(100) NOT NULL COMMENT '网站描述',
+  `link_rank` int(11) NOT NULL DEFAULT '0' COMMENT '用于列表排序',
+  `is_deleted` tinyint(4) NOT NULL DEFAULT '0' COMMENT '是否删除 0-未删除 1-已删除',
+  `create_time` datetime NOT NULL COMMENT '添加时间',
+  PRIMARY KEY (`link_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+/*Data for the table `tb_link` */
+
+insert  into `tb_link`(`link_id`,`link_type`,`link_name`,`link_url`,`link_description`,`link_rank`,`is_deleted`,`create_time`) values 
+(1,0,'lemochen','http://121.199.48.49/','lemochen',0,0,'2019-09-21 05:43:42');
+
+/*Table structure for table `tb_test` */
+
+DROP TABLE IF EXISTS `tb_test`;
+
+CREATE TABLE `tb_test` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `test_info` varchar(50) NOT NULL COMMENT '测试内容',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+/*Data for the table `tb_test` */
+
+insert  into `tb_test`(`id`,`test_info`) values 
+(1,'SpringBoot-MyBatis测试');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
